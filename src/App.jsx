df00734ce57504daf8552544cdbb38ab724bd84d
Kamisia <kamila.samczuk@gmail.com>
2024-03-04 11:03:52 +0100

    fetching data from FakeStoreAPI - axios, useQuery

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+const App = () => {
+  const fetchData = async () => {
+    const url = "https://fakestoreapi.com/products";
+    const response = await axios.get(url);
+    return response.data;
+  };
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["products"],
+    queryFn: () => fetchData(),
+  });
+  return (
+    <div>
+      <p>FakeShopApp</p>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error fetching weather data</div>}
+      {data && console.log(data)}
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from "react-dom/client";
+
+import App from "./App.jsx";
+import "./index.css";
+
+const queryClient = new QueryClient();
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
